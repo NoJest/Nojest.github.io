20804ed9fc641819e1fb7a3e2b53ffb40933afae
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-09-23 13:38:28 -0400

    added links images and a bit of styling

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 //Including code to add h2 element to the body element in DOM
 document.querySelector('body').appendChild(h2);
+//Adding event listener for buttons to be links
+// 
+document.addEventListener('DOMContentLoaded', () => {
+    const instagramButton = document.querySelector('.links button:nth-child(1)');
+    const spotifyButton = document.querySelector('.links button:nth-child(2)');
+  
+    instagramButton.addEventListener('click', () => {
+      window.open('https://Instagram.com/Nojest_', '_blank');
+    });
+  
+    spotifyButton.addEventListener('click', () => {
+      window.open('https://open.spotify.com/user/iohf8imf7f923nr073aw5trpq?si=59ebe575ce984d81', '_blank');
+    });
+  });
+  
